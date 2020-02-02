5fb45619a919cbe822ba09d11d78a9018e7d5d34
Ewok88 <mr.zhekabas@gmail.com>
2020-02-03 01:08:55 +0200

    first commit

---
+++ BlogTask/Controllers/GuestController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlogTask.Controllers
+{
+    public class GuestController : Controller
+    {
+
+        List<string[]> Reviews = new List<string[]>();
+
+        // GET: Guest
+
+        public ActionResult Index()
+        {
+            string[] rev1 = { "user1", "02.02.20", "Lorem ipsum dolor sit amet, consectetur adipiscing elit." };
+            string[] rev2 = { "user2", "01.02.20", "Lorem ipsum dolor sit amet, consectetur adipiscing elit." };
+            string[] rev3 = { "user3", "30.01.20", "Lorem ipsum dolor sit amet, consectetur adipiscing elit." };
+            string[] rev4 = { "user4", "30.01.20", "Lorem ipsum dolor sit amet, consectetur adipiscing elit." };
+
+            Reviews.Add(rev1);
+            Reviews.Add(rev2);
+            Reviews.Add(rev3);
+            Reviews.Add(rev4);
+
+            ViewBag.ReviewsBag = Reviews;
+
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult AddReview()
+        {
+            return View("~/Views/Guest/Index.cshtml");
+        }
+        [HttpPost]
+        public ViewResult AddReview(string name, string review)
+        {
+
+            string data = DateTime.Now.ToString();
+            string[] tempRev = { name, review, data };
+            Reviews.Add(tempRev);
+            ViewBag.ReviewsBag = Reviews;
+            return View("~/Views/Guest/Index.cshtml");
+        }
+
+       
+    }
+}
---
+++ BlogTask/Helpers/ListHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace BlogTask.Helpers
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayStringItems(this HtmlHelper html, string[] list, string type, string name)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+            foreach (string str in list)
+            {
+                TagBuilder listItemTag = new TagBuilder("li");
+                TagBuilder inputItemTag = new TagBuilder("input");
+                TagBuilder spanItemTag = new TagBuilder("span");
+                spanItemTag.SetInnerText(str);
+                inputItemTag.MergeAttribute("name", name);
+                inputItemTag.MergeAttribute("type", type);
+                inputItemTag.MergeAttribute("value", str);
+                listItemTag.AddCssClass("marker");
+                listItemTag.InnerHtml += inputItemTag.ToString();
+                listItemTag.InnerHtml += spanItemTag.ToString();
+
+
+
+                tag.InnerHtml += listItemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
